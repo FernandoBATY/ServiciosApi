4d2d970ae2be0bff308cc51a7c9590b48dca6a5d
Bryan Fernando Aguilar Torres <feraguilartorres216@gmail.com>
2025-06-02 07:28:33 -0600

    no hacer el ultimo pull de tania

--- TiendaServicio.Api.CarritoCompra/obj/Debug/net8.0/TiendaServicio.Api.CarritoCompra.AssemblyInfo.cs
+++ TiendaServicio.Api.CarritoCompra/obj/Debug/net8.0/TiendaServicio.Api.CarritoCompra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaServicio.Api.CarritoCompra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db36a1e6c57d6847badd899e748af2b8fd84ce67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624e7b14f6dcadcd018cce5a293693e1e9e38f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaServicio.Api.CarritoCompra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaServicio.Api.CarritoCompra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
